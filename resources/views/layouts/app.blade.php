134fb08e7c3476daae0dc2ecb9d3dcae94a39edb
rsbertoa90 <rsbertoa90@gmail.com>
2019-03-25 17:29:38 -0300

    eager google font load

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,20 @@
         <meta name="description" content="{{$meta->metadescription}}">
     @endif
 
-
-
+  
+    <script type="text/javascript">
+        WebFontConfig = {
+            google: { families: [ 'Raleway:400,700:latin','Open+Sans' ] }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })();
+    </script>
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
 
